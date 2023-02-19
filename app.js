18c3261a084543135e7136f976ea306085960b42
Gsus <js49831@gmail.com>
2023-02-19 10:41:43 -0500

    agregado el body parser a 'app.js' , carga de rutas de 'phones' y 'sio'

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-// packes
-const express = require('express');
-const config = require('config');
+// packages
+const express = require("express");
+const config = require("config");
+const bodyParser = require("body-parser");
 
-
-// app configuration 
+// app configuration
 const app = express();
-const controller = require('./controller/logic/phones.controller');
-const controllersio = require('./controller/logic/sio.controller');
+
+/** creation of JSON parser  */
+const jsonParser = bodyParser.json();
+const urlEncodedParser = bodyParser.urlencoded({
+    extended: true,
+});
+
+/** use of JSON parser  */
+app.use(jsonParser);
+app.use(urlEncodedParser);
+
+const controller = require("./controller/logic/phones.controller");
+const controllersio = require("./controller/logic/sio.controller");
 // port configuration
-const port = config.get("server-port")
+const port = config.get("server-port");
 
-app.get('/', (req, res) => {
-    res.send('api rest of cell phones and operating systems');
-})
+app.get("/", (req, res) => {
+    res.send("api rest of cell phones and operating systems");
+});
 
-app.get('/phones', (req, res) => {
-   controller.getAll(req, res);
-})
+app.get("/phones", (req, res) => {
+    controller.getAll(req, res);
+});
+
+// phones routes loading
+let phonesRoutes = require("./routes/phones.routes");
+phonesRoutes(app);
+
+// sio routes loading
+let sioRoutes = require("./routes/sio.routes");
+sioRoutes(app);
 
 app.listen(port, () => {
-    console.log('listening on port ' + port);
-})
+    console.log("listening on port " + port);
+});
